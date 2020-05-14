264c44ebb3cff96c849bdc922436f6e7275104d1
Andrew2432 <drew.devops@gmail.com>
2020-05-14 13:52:40 +0530

    Move data to a separate component

---
+++ src/data/itemData.js
@@ -0,0 +1,45 @@
+import image1 from '../../assets/images/image1.jpg';
+import image2 from '../../assets/images/image2.jpg';
+import image3 from '../../assets/images/image3.jpg';
+import image4 from '../../assets/images/image4.jpg';
+import image5 from '../../assets/images/image5.jpg';
+
+const itemData = [
+  {
+    id: 1,
+    image: image1,
+    text1: 'Hello',
+    text2: 'Hello',
+    text3: 'Hello',
+  },
+  {
+    id: 2,
+    image: image2,
+    text1: 'Hello',
+    text2: 'Hello',
+    text3: 'Hello',
+  },
+  {
+    id: 3,
+    image: image3,
+    text1: 'Hello',
+    text2: 'Hello',
+    text3: 'Hello',
+  },
+  {
+    id: 4,
+    image: image4,
+    text1: 'Hello',
+    text2: 'Hello',
+    text3: 'Hello',
+  },
+  {
+    id: 5,
+    image: image5,
+    text1: 'Hello',
+    text2: 'Hello',
+    text3: 'Hello',
+  },
+];
+
+export default itemData;
