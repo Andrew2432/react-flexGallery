2e8d10edec433a2c5da94a6261c9dbb785e55a2f
Andrew2432 <drew.devops@gmail.com>
2020-05-14 13:56:52 +0530

    Fix image src

--- src/data/itemData.js
+++ src/data/itemData.js
@@ -1,44 +1,44 @@
-import image1 from '../../assets/images/image1.jpg';
-import image2 from '../../assets/images/image2.jpg';
-import image3 from '../../assets/images/image3.jpg';
-import image4 from '../../assets/images/image4.jpg';
-import image5 from '../../assets/images/image5.jpg';
+import image1 from '../assets/images/image1.jpg';
+import image2 from '../assets/images/image2.jpg';
+import image3 from '../assets/images/image3.jpg';
+import image4 from '../assets/images/image4.jpg';
+import image5 from '../assets/images/image5.jpg';
 
 const itemData = [
   {
     id: 1,
     image: image1,
-    text1: 'Hello',
-    text2: 'Hello',
-    text3: 'Hello',
+    text1: 'A',
+    text2: 'Happy',
+    text3: 'Day',
   },
   {
     id: 2,
     image: image2,
-    text1: 'Hello',
-    text2: 'Hello',
-    text3: 'Hello',
+    text1: 'The',
+    text2: 'Birthday',
+    text3: 'Of',
   },
   {
     id: 3,
     image: image3,
-    text1: 'Hello',
-    text2: 'Hello',
-    text3: 'Hello',
+    text1: 'Blessings',
+    text2: 'To',
+    text3: 'You',
   },
   {
     id: 4,
     image: image4,
-    text1: 'Hello',
-    text2: 'Hello',
-    text3: 'Hello',
+    text1: 'Love',
+    text2: 'You',
+    text3: 'So Much',
   },
   {
     id: 5,
     image: image5,
-    text1: 'Hello',
-    text2: 'Hello',
-    text3: 'Hello',
+    text1: 'Dear',
+    text2: 'Andrew',
+    text3: 'Joel',
   },
 ];
 
